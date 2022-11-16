26571befffaad2bbfddd166db67d94fe4b3a4de0
PesnyaRadugi <mbarazgov02@gmail.com>
2022-11-16 11:09:45 +0300

    Made cutom editor for interactable objects
    Now can use Unity Events with interactable objects

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
+    public bool useEvents;
     [SerializeField] internal string promptMessage;
     
     public void BaseInteract()
     {
+        if (useEvents)
+        {
+            GetComponent<InteractionEvent>().OnInteract.Invoke();
+        }
         Interact();
     }
 
